80c3cf7a46e1da0f994062f3dc2ac95df4a71ea7
Gianmaria Micheli <giamma392@live.it>
2017-07-24 15:28:13 +0200

    Add method to get all files with a specific extension name, and show BW version of the image

--- HDRFusion/main.cpp
+++ HDRFusion/main.cpp
@@ -12,6 +12,10 @@
 #include <iostream>
 #include <math.h>
 
+#include<string.h>
+#include<fstream>
+#include<dirent.h>
+
 using namespace std;
 using namespace cv;
 
@@ -20,27 +24,58 @@ using namespace cv;
  */
 
 Mat toBN(Mat img);
+vector<string> listFile(const char* dirPath);
+bool has_suffix(const string& s, const string& suffix);
 
 int main(int argc, char** argv) {
-    Mat m = imread("lena.png", CV_LOAD_IMAGE_COLOR);
-    namedWindow("Test", WINDOW_AUTOSIZE);
-    imshow("Test", toBN(m));
-    waitKey(0);
+	vector<string> list = listFile(".");
+	for(int i = 0; i < list.size(); i++)
+	{
+		    Mat m = imread(list[i], CV_LOAD_IMAGE_COLOR);
+		    namedWindow(list[i], WINDOW_AUTOSIZE);
+		    imshow(list[i], toBN(m));
+		    waitKey(0);
+	}
+
     return 0;
 }
 
 Mat toBN(Mat img){
-    Mat res(img.rows, img.cols, CV_8UC1);
+    Mat res(img.rows, img.cols, CV_32FC1);
     for(int i = 0; i < img.rows; i++){
         for(int j = 0; j < img.cols; j++){
-            int new_pixel = 0;
+            float new_pixel = 0;
             Vec3b pixel = img.at<Vec3b>(i,j);
             for(int k = 0; k < 3; k++){
                 new_pixel += (int)pixel.val[k];
             }
-            new_pixel = (int)floor(((double)1/3) * new_pixel);
-            res.at<uchar>(i,j) = (uchar) new_pixel;
+            new_pixel = (((double)1/3) * new_pixel)/255;
+            cout << new_pixel << endl;
+            res.at<float>(i,j) = (float) new_pixel;
         }   
     }
     return res;
+}
+
+bool has_suffix(const string& s, const string& suffix)
+{
+    return (s.size() >= suffix.size()) && equal(suffix.rbegin(), suffix.rend(), s.rbegin());
+}
+
+vector<string> listFile(const char* dirPath){
+        DIR *pDIR;
+        vector<string> list;
+        struct dirent *entry;
+        if( pDIR=opendir(dirPath) ){
+                while(entry = readdir(pDIR)){
+                	if(has_suffix(entry->d_name, ".jpg"))
+                	{
+                		string s = entry->d_name;
+						list.push_back(s);
+                		cout << entry->d_name << "\n";
+                	}
+                }
+                closedir(pDIR);
+        }
+        return list;
 }
